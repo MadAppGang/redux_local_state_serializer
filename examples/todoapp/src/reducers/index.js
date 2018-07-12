ff70638e466f8042fc9c98b405ca1dd4ff349184
dprovodnikov <wireden@yandex.ru>
2018-07-12 23:17:49 +0300

    Adjust the example

--- examples/todoapp/src/reducers/index.js
+++ examples/todoapp/src/reducers/index.js
@@ -2,8 +2,6 @@ import { combineReducers } from 'redux';
 import { combineSerializers } from 'redux-local-state-serializer';
 import todos, { serializer as todosSerializer } from './todo';
 
-console.log(combineSerializers);
-
 export default combineReducers({ todos });
 
 export const serializer = combineSerializers({
